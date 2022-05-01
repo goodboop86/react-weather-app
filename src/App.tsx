baa0154ab8ea6b1c247794c868e15ed486234d74
goodboop86 <goodboop86@gmail.com>
2022-05-01 18:17:24 +0900

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Title } from './components/Title'
+import { Form } from './components/Form'
+import { Results } from './components/Results'
+import React, { useState } from 'react';
+import { type } from '@testing-library/user-event/dist/type';
 
+
+
+type ResultsStateType = {
+  country: string;
+  cityName: string;
+  temperature: string;
+  conditionText: string;
+  icon: string;
+}
 function App() {
+  const [city, setCity] = useState<string>("");
+  const [results, setResults] = useState<ResultsStateType>({
+    country: "",
+    cityName: "",
+    temperature: "",
+    conditionText: "",
+    icon: ""
+  });
+
+  const getWeather = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    fetch(`https://api.weatherapi.com/v1/current.json?key=83a56d9eba1141f5b1663354220105&q=${city}&aqi=no`)
+      .then(res => res.json())
+      .then(data => {
+        setResults({
+          country: data.location.country,
+          cityName: data.location.name,
+          temperature: data.current.temp_c,
+          conditionText: data.current.condition.text,
+          icon: data.current.condition.icon
+        })
+        setCity("");
+      }).catch(err => alert("error"))
+    console.log(city);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="container">
+        <Title />
+        <Form getWeather={getWeather} setCity={setCity} city={city} />
+        <Results results={results} />
+      </div>
     </div>
   );
 }
